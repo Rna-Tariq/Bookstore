5c89455c984d3fdd2f4e73220ce590d89d9c1823
Rna341 <rna.tariq.aboouf@gmail.com>
2024-02-09 15:42:24 +0200

    Enabled caching, Async, and configurations

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -2,8 +2,14 @@ package com.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@Configuration
+@EnableCaching
 @SpringBootApplication
+@EnableAsync
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
