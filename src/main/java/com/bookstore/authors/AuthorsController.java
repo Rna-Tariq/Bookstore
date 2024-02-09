b47f3d1d4fa088f84c4913bd5297f5771d5b9235
Rna341 <rna.tariq.aboouf@gmail.com>
2024-02-09 15:43:46 +0200

    Enhanced path value

--- src/main/java/com/bookstore/authors/AuthorsController.java
+++ src/main/java/com/bookstore/authors/AuthorsController.java
@@ -11,7 +11,7 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 
 @RestController
-@RequestMapping("/authors")
+@RequestMapping("/author")
 public class AuthorsController {
 
     @Autowired
